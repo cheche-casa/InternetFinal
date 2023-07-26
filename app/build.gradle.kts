20b48f4ceefd3a88a8e2ccfcd58065ee113b6d40
cheche-casa <cheche.casa@outlook.es>
2023-07-26 21:02:27 +0200

    parte 3 con reservas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,9 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
 
